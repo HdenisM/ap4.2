a1f0a4894340a2c9c4e66ac93d5c989fe266d060
HdenisM <115537241+HdenisM@users.noreply.github.com>
2023-05-28 22:45:20 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+$("#image").on('click', function() {
+  $(".fill").toggleClass('scale')
+  $(".station").toggleClass('hide')
+  $(".krug").toggleClass('hide')
+  $(".yellow").toggleClass('hide')
+  $(".blue").toggleClass('hide')
+  $(".red").toggleClass('hide')
+  $(".violet").toggleClass('hide')
+  $(".green").toggleClass('hide')
+
+  $(".top").toggleClass('top_final')
+});
