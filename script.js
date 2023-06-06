75c63e2a3e47c1c377c98a962f39d82e21eac754
HdenisM <115537241+HdenisM@users.noreply.github.com>
2023-06-06 15:23:43 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -9,4 +9,51 @@ $("#image").on('click', function() {
   $(".green").toggleClass('hide')
 
   $(".top").toggleClass('top_final')
+  $(".menue").toggleClass('menue_final')
+});
+
+
+const el = document.getElementById('container1');
+const hiddenDiv = document.getElementById('hidden-word1');
+el.addEventListener('mouseover', function handleMouseOver() {
+  hiddenDiv.style.visibility = 'visible';
+});
+el.addEventListener('mouseout', function handleMouseOut() {
+  hiddenDiv.style.visibility = 'hidden';
+});
+
+const el2 = document.getElementById('container2');
+const hiddenDiv2 = document.getElementById('hidden-word2');
+el2.addEventListener('mouseover', function handleMouseOver() {
+  hiddenDiv2.style.visibility = 'visible';
+});
+el2.addEventListener('mouseout', function handleMouseOut() {
+  hiddenDiv2.style.visibility = 'hidden';
+});
+
+const el3 = document.getElementById('container3');
+const hiddenDiv3 = document.getElementById('hidden-word3');
+el3.addEventListener('mouseover', function handleMouseOver() {
+  hiddenDiv3.style.visibility = 'visible';
+});
+el3.addEventListener('mouseout', function handleMouseOut() {
+  hiddenDiv3.style.visibility = 'hidden';
+});
+
+const el4 = document.getElementById('container4');
+const hiddenDiv4 = document.getElementById('hidden-word4');
+el4.addEventListener('mouseover', function handleMouseOver() {
+  hiddenDiv4.style.visibility = 'visible';
+});
+el4.addEventListener('mouseout', function handleMouseOut() {
+  hiddenDiv4.style.visibility = 'hidden';
+});
+
+const el5 = document.getElementById('container5');
+const hiddenDiv5 = document.getElementById('hidden-word5');
+el5.addEventListener('mouseover', function handleMouseOver() {
+  hiddenDiv5.style.visibility = 'visible';
+});
+el5.addEventListener('mouseout', function handleMouseOut() {
+  hiddenDiv5.style.visibility = 'hidden';
 });
